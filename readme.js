b01598e71ea1dce52191039b3d7a7299e71a968f
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2019-01-29 22:45:41 +0100

    readme.js

--- readme.js
+++ readme.js
@@ -6,6 +6,7 @@ you will then a list of scripts with names like 'for-loop-let.js'
 
 to open an example type load('name-of-file.js'); into the console
 study the console logs & the source to practice tracing code
-  (to the right of each console.log is the line it printed from)
+  to the right of each console.log is the line of code that printed it
+  click on the underlined file name to highlight that line in source
   
 `);
